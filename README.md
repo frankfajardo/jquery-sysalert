21826ad24c22d22c200d5cb786947c8fde6156e3
ufajafr <frank.fajardo@pearson.com>
2015-07-03 16:51:41 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# sysAlert.js - a jQuery plugin #
+
+
+This jQuery plugin is made to allow quickly adding user notifications to a website or webpage for any scheduled service interruptions.
+
+
+## Version ##
+
+0.0.1
+
+
+## Getting Started ##
+
+1. Copy the content of the `dist` folder into your project.
+2. Add references to the js and css files to the web pages that you want to show an system alert message. Also add reference to [jQuery], and optionally to the [FontAwesome] css. If you do not want to use FontAwesome, that's okay. Your system alert message will simply not show the FontAwesome bullhorn icon on the left of the system alert message.
+3. Define an element that will contain the system alert message. 
+4. Call sysAlert as follow: `$('your element selector').sysAlert({options})`
+
+## Options ##
+
+* `sourceUrl` - This is the URL to get your json file which defines your system alert. Defaults to `'/sysalert.json'`
+* `sourceMethod` - The http method to use to retrieve your json file. Defaults to `'GET'`
+* `allowHtml` - This indicates if your system alert has embeded HTML tags. Defaults to `false`,
+* `complete` - This is the function to call on completion of sysAlert
+
+## JSON Content ##
+
+The JSON file may be named any name you like. But these are the properties sysAlert.js will look for:
+* `shortAlert` - *This is mandatory.* This is the alert message that is initially shown on the page. If `allowHtml` is `true`, this is interpreted as an HTML markup.
+* `longAlert` - *This is optional.* This is the alert message shown when the user expands the alert to read more information. If `allowHtml` is `true`, this is interpreted as an HTML markup.
+* `postFrom` - *This is optional.* This indicates when the alert becomes effective. It can be a date such as `2015-07-01` or a date\time such as `2015-07-01T00:00:01`. It not specified, it is assumed the alert is effective immediately.
+* `postUpto` - *This is mandatory.* This indicates when the alert expires. It has the same format as `postFrom`.
+
+## To Do ##
+
+I hope to come up with a version of this plugin that can be packaged, when I find time.
+
+
---
+++ dist/scripts/sysalert.js
@@ -0,0 +1,69 @@
+﻿
+(function ($) {
+
+    $.fn.sysAlert = function (options) {
+
+        // Default options.
+        var settings = $.extend({
+            sourceUrl: '/sysalert.json',
+            sourceMethod: 'GET',
+            allowHtml: false,
+            complete: function () {}
+        }, options);
+
+        var allowHtml = settings.allowHtml;
+
+        var $self = $(this);
+        var $sa = $('<span class="sa sa-short"></span>');
+        var $icon = $('<span class="sa-icon"><i class="fa fa-bullhorn"></i></span>');
+        var $short = $('<span class="sa-shorttext"></span>');
+        var $long = $('<span class="sa-longtext"></span>');
+        var $toggle = $('<span class="sa-text-toggle"></span>');
+
+        $.ajax({
+            url: settings.sourceUrl,
+            type: settings.sourceMethod,
+            dataType: 'json',
+            success: function (sa) {
+                var from = sa.postFrom && new Date(sa.postFrom) || new Date();
+                var upto = new Date(sa.postUpto);
+                var today = new Date();
+                var shortAlert = sa.shortAlert || '';
+                var longAlert = sa.longAlert || '';
+                var hasLongText = (typeof longAlert === 'string') && (longAlert !== '');
+                if (today >= from && today <= upto) {
+                    if (hasLongText) {
+                        $sa.appendTo($self).append($icon).append($short).append($long).append($toggle);
+                        if (allowHtml) {
+                            $short.html(shortAlert);
+                            $long.html(longAlert);
+                        }
+                        else {
+                            $short.text(shortAlert);
+                            $long.text(longAlert);
+                        }
+                        $toggle.on('click', function (e) {
+                            if ($sa.is('.sa-short')) {
+                                $sa.removeClass('sa-short').addClass('sa-long');
+                            }
+                            else {
+                                $sa.removeClass('sa-long').addClass('sa-short');
+                            }
+                            e.preventDefault();
+                        })
+                    }
+                    else {
+                        $sa.appendTo($self).append($icon).append($short);
+                        allowHtml && $short.html(shortAlert) || !allowHtml && $short.text(shortAlert);
+                    }
+                    //$self.show();
+                    settings.complete.call(this);
+                }
+            },
+            error: function () {
+                settings.complete.call(this);
+            }
+        });
+    };
+
+}(jQuery));
